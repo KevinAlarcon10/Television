7731ff8149c237e2a96d0d3e7966cb2118a4fdb8
AlarconKevin10 <alarconkevin300@gmail.com>
2024-04-05 07:12:07 -0500

    quiz

--- televisor.java
+++ televisor.java
@@ -1,56 +1,51 @@
 
-public class televisor {
+public class television {
 
 	 private String marca , tamaño, volumen,canal ,entradas , encendido;
-//c
+
 	 public void encender () {};
 	 public void volmas () {};
 	 public void volmen () {} ;
 	 public void canalmas () {};
-	 public void canalmen () {}
-	
-	 //g y s
+	 public void canalmen () {};
 	 
-	 public String getMarca() {
-		return marca;
-	}
-	public void setMarca(String marca) {
-		this.marca = marca;
-	}
-	public String getTamaño() {
-		return tamaño;
-	}
-	public void setTamaño(String tamaño) {
-		this.tamaño = tamaño;
-	}
-	public String getVolumen() {
-		return volumen;
-	}
-	public void setVolumen(String volumen) {
-		this.volumen = volumen;
 	}
 	public String getCanal() {
-		return canal;
+		   return canal;
 	}
 	public void setCanal(String canal) {
-		this.canal = canal;
+		   this.canal = canal;
 	}
 	public String getEntradas() {
-		return entradas;
+		   return entradas;
 	}
 	public void setEntradas(String entradas) {
-		this.entradas = entradas;
+		   this.entradas = entradas;
 	}
 	public String getEncendido() {
-		return encendido;
+		   return encendido;
 	}
 	public void setEncendido(String encendido) {
-		this.encendido = encendido;
-	};
+		    this.encendido = encendido;
+	}
+	public String getMarca() {
+			return marca;
+		}
+    public void setMarca(String marca) {
+			this.marca = marca;
+		}
+	public String getTamaño() {
+			return tamaño;
+		}
+	public void setTamaño(String tamaño) {
+			this.tamaño = tamaño;
+		}
+	public String getVolumen() {
+			return volumen;
+		}
+	public void setVolumen(String volumen) {
+			this.volumen = volumen;
 	 
 
-	
-	
-	
 }
 
